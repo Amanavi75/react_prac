432f1cd62d68ef6cfa126bd7ebbd3553de83a32f
amanavi75 <amanavi75@gmail.com>
2023-12-15 12:34:20 +0530

    configured the tailwind css for the project

---
+++ props/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",  // this will search our content in the html and required script File 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
