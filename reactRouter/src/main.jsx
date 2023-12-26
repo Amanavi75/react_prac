7da664c753138440a92833d053d0dee2e99a9c28
amanavi75 <amanavi75@gmail.com>
2023-12-26 11:57:00 +0530

    added the about and contact functionaity using ReactRouter

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import {RouterProvider,createBrowserRouter} from 'react-router-dom'
+import {Route, RouterProvider,createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
 import  Layout from '../src/Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
 
-const router = createBrowserRouter([
+/*const router = createBrowserRouter([
   {
     path: '/',
     element:<Layout/>,
@@ -20,7 +21,22 @@ const router = createBrowserRouter([
     ]
   }
  
-])
+])*/
+
+
+//* additional method to create router 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+     <Route path='' element={<Home/>}/>
+     <Route path='about' element={<About/>}/>
+     <Route path='Contact' element={<Contact/>}/>
+
+
+    </Route>
+  )
+)
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
